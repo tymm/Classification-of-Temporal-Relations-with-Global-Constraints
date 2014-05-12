ba8b90ed60938614dc67adc691d4ebe5d2dbb72b
Timm <tymmm1@gmail.com>
2014-05-12 12:19:52 +0200

    Basic class structure

--- parser.py
+++ parser.py
@@ -1,15 +1,25 @@
 import os
+from lxml import etree
 
 class parser:
     def __init__(self, *corpora):
         self.corpora = corpora
+        # Hols all textfile objects
+        self.text_objects = []
         self._parse(self.corpora)
 
     def _parse(self, corpora):
         # Holds all corpora files
         files = []
+
+        # Get all files
         for corpus in corpora:
             files = files + self._fetch_files(corpus)
+
+        # Parse all files
+        for file in files:
+            # Mapping xml data to python objects
+            self.text_objects.append(self._parse_xml_to_objects(file))
 
     def _fetch_files(self, directory):
         files = []
@@ -22,6 +32,15 @@ class parser:
             files.append(directory + file)
 
         return files
+
+    def _parse_xml_to_objects(self, file):
+        tree = etree.parse(file)
+        root = tree.getroot()
+
+        for text in root.iterdescendants("TEXT"):
+            for event in text.iterdescendants("EVENT"):
+                print event.text
+
 
 
 if __name__ == "__main__":
