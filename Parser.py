6999729f2b7b826f5586b06d4a99c13255cbda1b
Timm <tymmm1@gmail.com>
2014-05-13 13:03:27 +0200

    Changed Parser class to parse just one file at a time instead of all corpora files at once

--- Parser.py
+++ Parser.py
@@ -7,39 +7,17 @@ from parsexml.event import Event
 import re
 
 class Parser:
-    def __init__(self, *corpora):
-        self.corpora = corpora
-        # Hols all textfile objects
-        self.text_objects = []
-        self._parse(self.corpora)
+    def __init__(self, corpus_file):
+        self.filename = corpus_file
+        self.text_object = None
+        self._parse(self.filename)
 
-    def get_text_objects(self):
-        return self.text_objects
+    def get_text_object(self):
+        return self.text_object
 
-    def _parse(self, corpora):
-        # Holds all corpora files
-        files = []
-
-        # Get all files
-        for corpus in corpora:
-            files = files + self._fetch_files(corpus)
-
-        # Parse all files
-        for file in files:
-            # Mapping xml data to python objects
-            self.text_objects.append(self._parse_xml_to_objects(file))
-
-    def _fetch_files(self, directory):
-        files = []
-
-        # Append '/' if there is no at the end of directory string
-        if not directory.endswith('/'):
-            directory = directory + '/'
-
-        for file in os.listdir(directory):
-            files.append(directory + file)
-
-        return files
+    def _parse(self, filename):
+        # Mapping xml data to python objects
+        self.text_object = self._parse_xml_to_objects(filename)
 
     def _stringify_children(self, node):
         # Transfer sub tree into string
@@ -70,12 +48,6 @@ class Parser:
             target_eid = relation.get("relatedToEventInstance")
             relation_type = relation.get("relType")
 
-        # Find source event
-        source_obj = self._find_event_by_eid(source_eid)
-
-        # Find target event
-        target_obj = self._find_event_by_eid(target_eid)
-
     def _parse_xml_to_objects(self, file):
         tree = etree.parse(file)
         root = tree.getroot()
@@ -96,4 +68,4 @@ class Parser:
 
 
 if __name__ == "__main__":
-    a = Parser("data/training/TE3-Silver-data/", "data/training/TBAQ-cleaned/AQUAINT/", "data/training/TBAQ-cleaned/TimeBank/")
+    a = Parser("data/training/TE3-Silver-data/AFP_ENG_19970401.0006.tml")
