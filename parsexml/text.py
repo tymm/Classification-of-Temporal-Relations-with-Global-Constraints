73daaf1eb5a8d71b44c51beb70b7bc6795bca8b5
Timm <tymmm1@gmail.com>
2014-05-13 13:55:32 +0200

    Added relations

--- parsexml/text.py
+++ parsexml/text.py
@@ -3,12 +3,16 @@ class Text:
         self.file = file
         self.text = text
         self.events = []
+        self.relations = []
 
     def set_text(self, text):
         self.text = text
 
     def append_event(self, event):
         self.events.append(event)
+
+    def append_relation(self, relation):
+        self.relations.append(relation)
 
     def find_event_by_eid(self, eid):
         for event in self.events:
