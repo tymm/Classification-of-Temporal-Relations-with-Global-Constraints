005bab79f11396ee30c2b40e14b5a7123f2a136a
Timm <tymmm1@gmail.com>
2014-05-13 19:36:09 +0200

    Event-event relationship mapping works now

--- Parser.py
+++ Parser.py
@@ -4,20 +4,35 @@ from lxml.etree import tostring
 from itertools import chain
 from parsexml.text import Text
 from parsexml.event import Event
+from parsexml.relation import Relation
+from parsexml.relationtype import RelationType
 import re
 
 class Parser:
     def __init__(self, corpus_file):
         self.filename = corpus_file
-        self.text_object = None
+        self.text_obj = Text(self.filename)
         self._parse(self.filename)
 
     def get_text_object(self):
-        return self.text_object
+        return self.text_obj
 
     def _parse(self, filename):
-        # Mapping xml data to python objects
-        self.text_object = self._parse_xml_to_objects(filename)
+        """Mapping xml data to python objects."""
+        tree = etree.parse(filename)
+        root_node = tree.getroot()
+
+        text_node = root_node.find("TEXT")
+
+        # Get text and pass it to Text object
+        extracted_text = self._extract_text(text_node)
+        self.text_obj.set_text(extracted_text)
+
+        # Create Event objects and link them to the Text object
+        self._create_event_objects(text_node, root_node)
+
+        # Create Relation objects and link them
+        self._create_relation_objects(root_node)
 
     def _stringify_children(self, node):
         # Transfer sub tree into string
@@ -29,42 +44,48 @@ class Parser:
 
         return text
 
-    def _extract_text(self, xml_text_obj):
-        text = self._stringify_children(xml_text_obj)
+    def _extract_text(self, node):
+        text = self._stringify_children(node)
         return text
 
-    def _create_event_objects(self, text, text_obj):
-        for event in text.iterdescendants("EVENT"):
-            event_obj = Event(event.get("eid"), text_obj, event.text)
-            text_obj.append_event(event_obj)
+    def _create_event_objects(self, text_node, root_node):
+        for event in text_node.iterdescendants("EVENT"):
+            eid = event.get("eid")
+            eiid = None
 
-        return text_obj
+            # Get eeid for event
+            for instance in root_node.iterdescendants("MAKEINSTANCE"):
+                instance_eid = instance.get("eventID")
+                if instance_eid == eid:
+                    eiid = instance.get("eiid")
+                    break
 
-    def _create_relation_objects(self, root):
-        """Must be called after _create_event_objects"""
-        for relation in root.iterdescendants("TLINK"):
+            # Create Event object and append it to Text object
+            event_obj = Event(eid, eiid, self.text_obj, event.text)
+            self.text_obj.append_event(event_obj)
+
+    def _create_relation_objects(self, root_node):
+        """Must be called after _create_event_objects."""
+        for relation in root_node.iterdescendants("TLINK"):
             lid = relation.get("lid")
-            source_eid = relation.get("eventInstanceID")
-            target_eid = relation.get("relatedToEventInstance")
+            source_eiid = relation.get("eventInstanceID")
+            target_eiid = relation.get("relatedToEventInstance")
             relation_type = relation.get("relType")
 
-    def _parse_xml_to_objects(self, file):
-        tree = etree.parse(file)
-        root = tree.getroot()
+            # Get relation_type_id
+            relation_type_id = RelationType.get_id(relation_type)
 
-        text = root.find("TEXT")
+            # Find source event
+            source_event_obj = self._find_event_by_eiid(source_eiid)
 
-        extracted_text = self._extract_text(text)
+            # Find target event
+            target_event_obj = self._find_event_by_eiid(target_eiid)
 
-        text_obj = Text(file, extracted_text)
+            relation_obj = Relation(lid, self.text_obj, source_event_obj, target_event_obj, relation_type_id)
+            self.text_obj.append_relation(relation_obj)
 
-        # Create Event objects and link them to the Text object
-        self._create_event_objects(text, text_obj)
-
-        # Create Relation objects and link them
-        self._create_relation_objects(root)
-
-        return text_obj
+    def _find_event_by_eiid(self, eiid):
+        return self.text_obj.find_event_by_eiid(eiid)
 
 
 if __name__ == "__main__":
