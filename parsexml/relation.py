cef4859c6a9d3ed3764e5e2f54499569c299b49c
Timm <tymmm1@gmail.com>
2014-05-13 13:33:52 +0200

    Making it clearer that Relation needs source and target event objects

--- parsexml/relation.py
+++ parsexml/relation.py
@@ -1,7 +1,7 @@
 class Relation:
-    def __init__(self, lid, text_obj, source, target, relation_type):
+    def __init__(self, lid, text_obj, source_event_obj, target_event_obj, relation_type):
         self.lid = lid
         self.parent_node = text_obj
-        self.source_event = source
-        self.target_event = target
+        self.source_event = source_event_obj
+        self.target_event = target_event_obj
         self.relation_type = relation_type
