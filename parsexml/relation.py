ae5cd3f42758f76e3cc52404138aaeb8b3080fe9
Timm <tymmm1@gmail.com>
2014-05-14 19:38:02 +0200

    Check if event-timex or event-event relation in constructor

--- parsexml/relation.py
+++ parsexml/relation.py
@@ -1,11 +1,15 @@
+from parsexml.timex import Timex
+
 class Relation:
-    def __init__(self, lid, text_obj, source_obj, target_obj, relation_type_id, timex=False):
+    def __init__(self, lid, text_obj, source_obj, target_obj, relation_type_id):
         self.lid = lid
         self.parent = text_obj
         self.source = source_obj
         self.target = target_obj
         self.relation_type = relation_type_id
-        self.timex = timex
+        self.is_timex = None
+
+        self._check_timex()
 
     def is_event_timex(self):
         if self.timex:
@@ -15,3 +19,12 @@ class Relation:
 
     def is_event_event(self):
         return not self.is_event_timex()
+
+    def _check_timex(self):
+        """Check if this relation is an event-event relation or an event-timex relation."""
+        print type(self.target)
+        if type(self.source) is Timex or type(self.target) is Timex:
+            self.is_timex = True
+        else:
+            self.is_timex = False
+
