a9897ef0bb959e6250e653e411acc8a560a30fc5
Timm <tymmm1@gmail.com>
2014-05-27 00:19:16 +0200

    Returning POS tag

--- helper/tagger.py
+++ helper/tagger.py
@@ -5,8 +5,9 @@ class StanfordNLP:
     def __init__(self):
         self.server = jsonrpc.ServerProxy(jsonrpc.JsonRpc20(), jsonrpc.TransportTcpIp(addr=("127.0.0.1", 8080)))
 
-    def parse(self, text):
+    def parse(self, text, position):
         try:
             response = loads(self.server.parse(text))
+            return response["sentences"][0]["words"][position][1]["PartOfSpeech"]
         except jsonrpc.RPCTransportError:
             print "Error: Please start the StanfordNLP server."
