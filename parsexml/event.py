9620c7da088dba572b0f979622931e569721f55f
Timm <tymmm1@gmail.com>
2014-06-02 19:53:17 +0200

    Rename attribute parent_node to parent since the '_node' is used for lxml objects

--- parsexml/event.py
+++ parsexml/event.py
@@ -4,7 +4,7 @@ class Event(object):
     def __init__(self, eid, eiid, text_obj, text, sentence, position_in_sentence, e_class, tense, aspect, polarity, pos, modality):
         self.eid = eid
         self.eiid = eiid
-        self.parent_node = text_obj
+        self.parent = text_obj
         self.text = text
         self.sentence = sentence
         self.position_in_sentence = position_in_sentence
