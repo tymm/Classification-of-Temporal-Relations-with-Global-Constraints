9606dca7abcb6ef8e4e4c2dd66b5aa406711e6e0
Timm <tymmm1@gmail.com>
2014-06-02 19:56:36 +0200

    Rename attribute 'value' to 'text' since it is the same for Event objects

--- parsexml/timex.py
+++ parsexml/timex.py
@@ -1,9 +1,9 @@
 from helper.tagger import StanfordNLP
 
 class Timex(object):
-    def __init__(self, tid, type, value, sentence, position_in_sentence):
+    def __init__(self, tid, type, text, sentence, position_in_sentence):
         self.tid = tid
         self.type = type
-        self.value = value
+        self.text = text
         self.sentence = sentence
         self.position_in_sentence = position_in_sentence
