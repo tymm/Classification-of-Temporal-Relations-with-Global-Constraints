44d2fbdd1090925baa9c86fadd973a6febfd8a8e
Timm <tymmm1@gmail.com>
2014-06-15 16:14:58 +0200

    There was a bug which caused wrong sentence extraction.

--- parsexml/sentence.py
+++ parsexml/sentence.py
@@ -22,31 +22,40 @@ class Sentence:
         left = self._get_left_part(node)
         right = self._get_right_part(node)
 
-        if left and right:
-            text = left + right
-        elif left:
-            text = left
-        elif right:
-            text = right
+        left_sentence = self._get_most_right_sentence(left)
+        right_sentence = self._get_most_left_sentence(right)
 
-        return self._strip_dots(text)
+        if left_sentence and right_sentence:
+            text = left_sentence + right_sentence
+        elif left_sentence:
+            text = left_sentence
+        elif right_sentence:
+            text = right_sentence
 
-    def _strip_dots(self, text):
-        if "." in text:
-            string = None
-            parts = text.split(".")
+        return self._strip(text) + "."
 
-            if len(parts) == 3:
-                string = parts[1]
-            elif len(parts) == 2:
-                string = parts[0]
-            else:
-                string = parts[0]
+    def _get_most_right_sentence(self, sentences):
+        if sentences:
+            parts = sentences.split(".")
 
-            string = string.strip() + "."
-            return string
+            return parts[-1]
         else:
-            return text
+            return ""
+
+    def _get_most_left_sentence(self, sentences):
+        if sentences:
+            parts = sentences.split(".")
+
+            return parts[0]
+        else:
+            return ""
+
+    def _strip(self, text):
+        text = text.strip()
+        text = text.strip('.')
+        text = text.strip('\n')
+
+        return text
 
     def _get_left_part(self, node):
         beginning = None
@@ -60,8 +69,14 @@ class Sentence:
             else:
                 return self._get_left_part(prev_node) + beginning
         else:
-            # We are at the beginning
-            return node.getparent().text
+            # We are either at the beginning or in a headline
+            parent_text = node.getparent().text
+
+            # Assumption: Parents of headlines (<DCT></DCT>) don't have text
+            if parent_text:
+                return parent_text
+            else:
+                return ""
 
     def _get_right_part(self, node):
         if node is not None:
