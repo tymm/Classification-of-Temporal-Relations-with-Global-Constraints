05c31e4cb7f11a3332ad2827c864ea5bd31213f1
Timm <tymmm1@gmail.com>
2014-06-16 16:51:30 +0200

    Make FakeSentence comparable

--- parsexml/fakesentence.py
+++ parsexml/fakesentence.py
@@ -2,3 +2,9 @@ class FakeSentence(object):
     """For being able to compare text with Sentence objects."""
     def __init__(self, text):
         self.text = text
+
+    def __eq__(self, other):
+        return self.text == other.text
+
+    def __hash__(self):
+        return hash(self.text)
