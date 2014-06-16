217ffcd83ddc4304130a88289edb93ddcee8b729
Timm <tymmm1@gmail.com>
2014-06-16 18:12:49 +0200

    Adding _strip() method which gets called on the input text for removing '\n', '.' and whitespaces

--- parsexml/fakesentence.py
+++ parsexml/fakesentence.py
@@ -1,7 +1,7 @@
 class FakeSentence(object):
     """For being able to compare text with Sentence objects."""
     def __init__(self, text):
-        self.text = text
+        self.text = self._strip(text) + "."
 
     def __eq__(self, other):
         return self.text == other.text
@@ -11,3 +11,10 @@ class FakeSentence(object):
 
     def __str__(self):
         return u"FakeSentence Object: %s" % (self.text)
+
+    def _strip(self, text):
+        text = text.strip()
+        text = text.strip('.')
+        text = text.strip('\n')
+
+        return text
