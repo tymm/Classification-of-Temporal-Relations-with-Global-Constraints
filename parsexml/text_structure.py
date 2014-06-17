6b1c5af9bbf25ceda55edff6e51828bd3173afda
Timm <tymmm1@gmail.com>
2014-06-17 15:06:03 +0200

    Building the text structure works fine now. Tests are running through.

--- parsexml/text_structure.py
+++ parsexml/text_structure.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 from parsexml.sentence import Sentence
+from parsexml.fakesentence import FakeSentence
 from lxml import etree
 
 class Text_structure:
@@ -88,6 +89,7 @@ class Text_structure:
 
     def _build_structure(self):
         for entity_node in self._entity_nodes_ordered:
+            # Problem: Not all sentences have an entity
             sentence = Sentence(entity_node)
             # Get entity object by lxml object
             entity = self._get_entity_by_node(entity_node)
@@ -98,3 +100,78 @@ class Text_structure:
             # Sentence is already known. Let's append this entity to the already known sentence
             else:
                 self.structure[sentence].append(entity)
+
+        # Add sentences which don't have entities
+        self._add_sentences_without_entities()
+
+    def _add_sentences_without_entities(self):
+        """Strategy:
+        Get the whole text
+        Split the whole text into sentences
+        If a sentence is not yet in self.structure then add it to the right position
+        """
+        all_text = self._get_all_text()
+
+        # Split into single sentences
+        sentences = all_text.split(".")
+
+        # Create FakeSentence objects for being able to compare with Sentence objects
+        fake_sentences = []
+        for sentence in sentences:
+            fake_sentences.append(FakeSentence(sentence))
+
+        index_of_last_real_sentence = 0
+        for fakesentence in fake_sentences:
+            if fakesentence in self.structure.keys():
+                # This sentence is already in the structure; it contains an entity
+                index_of_last_real_sentence = self.structure.keys().index(fakesentence)
+            else:
+                # This sentence is not yet in the structure
+                # Append it after position to self.structure
+                self._append_fakesentence_to_structure_after_position(fakesentence, index_of_last_real_sentence)
+
+    def _get_all_text(self):
+        tree = etree.parse(self.filename)
+        root_node = tree.getroot()
+
+        text_node = root_node.find("TEXT")
+
+        # Get the very beginning of the text
+        if text_node.text:
+            text = text_node.text
+
+        # Getting the rest
+        entity = text_node[0]
+
+        while entity is not None:
+            text = text + entity.text + entity.tail
+            entity = entity.getnext()
+
+        return text
+
+    def _append_fakesentence_to_structure_after_position(self, fakesentence, position):
+        """Append at position. If there are FakeSentence objects after position then put item at the end of them and before the next real Sentence object."""
+        # Convert to list
+        l = self.structure.items()
+
+        position_to_insert = self._find_position_to_insert_sentence(position)
+
+        # Insert (item, []) to be consistent with the definition of structure
+        l.insert(position_to_insert, (fakesentence, []))
+
+        # Update structure
+        self.structure = OrderedDict(l)
+
+    def _find_position_to_insert_sentence(self, starting_position):
+        # Search for first non FakeSentence objects after position
+        position_to_insert = None
+
+        # List of sentences (entities are not interesting here)
+        list_of_sentences = self.structure.keys()
+
+        for sentence in list_of_sentences[(starting_position+1):]:
+            if type(sentence) is Sentence:
+                position_to_insert = list_of_sentences.index(sentence)
+                return position_to_insert
+
+        return len(list_of_sentences)
