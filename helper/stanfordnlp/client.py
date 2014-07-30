7e1a4400f50503341cd53ecffb37d1a5278d7dcc
Timm <tymmm1@gmail.com>
2014-07-30 22:11:39 +0200

    Changed intent

--- helper/stanfordnlp/client.py
+++ helper/stanfordnlp/client.py
@@ -4,9 +4,8 @@ from pprint import pprint
 
 class StanfordNLP:
     def __init__(self):
-        self.server = ServerProxy(JsonRpc20(),
-                                  TransportTcpIp(addr=("127.0.0.1", 8080)))
-    
+        self.server = ServerProxy(JsonRpc20(), TransportTcpIp(addr=("127.0.0.1", 8080)))
+
     def parse(self, text):
         return json.loads(self.server.parse(text))
 
