3b3d7298d6c70c2655700c58d49d69e4317000d7
Timm <tymmm1@gmail.com>
2014-08-05 19:44:44 +0200

    Changed port

--- helper/stanfordnlp/client.py
+++ helper/stanfordnlp/client.py
@@ -4,7 +4,7 @@ from pprint import pprint
 
 class StanfordNLP:
     def __init__(self):
-        self.server = ServerProxy(JsonRpc20(), TransportTcpIp(addr=("127.0.0.1", 8080)))
+        self.server = ServerProxy(JsonRpc20(), TransportTcpIp(addr=("127.0.0.1", 8081)))
 
     def parse(self, text):
         return json.loads(self.server.parse(text))
