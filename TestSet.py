31f2cda7065dd1b304366ac6ee8b35b9a36c994a
Timm <tymmm1@gmail.com>
2014-08-10 23:36:57 +0200

    Outsourced stuff to Set.py

--- TestSet.py
+++ TestSet.py
@@ -6,23 +6,17 @@ class TestSet(Set):
     def __init__(self, load=True, *corpora):
         Set.__init__(self, load, *corpora)
 
-        self._ground_truth_event_event_rels = []
-
-        self._ground_truth_event_timex_rels = []
-
-        self._set_ground_truth_data()
-
     def classify_existing_event_event_relations(self, classifier, lemma, token):
-        features = self._get_feature_data(self._ground_truth_event_event_rels, lemma, token)
+        features = self._get_feature_data(self._event_event_rels, lemma, token)
         y_predicted = classifier.predict(features)
-        y_truth = self._extract_classes(self._ground_truth_event_event_rels)
+        y_truth = self._extract_classes(self._event_event_rels)
 
         return self._evaluation(y_predicted, y_truth)
 
     def classify_existing_event_timex_relations(self, classifier, lemma, token):
-        features = self._get_feature_data(self._ground_truth_event_timex_rels, lemma, token)
+        features = self._get_feature_data(self._event_timex_rels, lemma, token)
         y_predicted = classifier.predict(features)
-        y_truth = self._extract_classes(self._ground_truth_event_timex_rels)
+        y_truth = self._extract_classes(self._event_timex_rels)
 
         return self._evaluation(y_predicted, y_truth)
 
@@ -45,14 +39,6 @@ class TestSet(Set):
             y.append(relation.get_result())
 
         return y
-
-    def _set_ground_truth_data(self):
-        for text_obj in self.text_objects:
-            for relation in text_obj.relations:
-                if relation.is_event_event():
-                    self._ground_truth_event_event_rels.append(relation)
-                elif relation.is_event_timex():
-                    self._ground_truth_event_timex_rels.append(relation)
 
     def _evaluation(self, predicted, truth):
         true_pos = 0
