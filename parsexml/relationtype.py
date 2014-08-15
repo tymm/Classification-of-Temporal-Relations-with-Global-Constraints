4f1077a84271b340a2566e80f6aa8e4eaa4ae132
Timm <tymmm1@gmail.com>
2014-08-15 21:31:44 +0200

    Added method which takes relation id and return relation string.

--- parsexml/relationtype.py
+++ parsexml/relationtype.py
@@ -16,6 +16,41 @@ class RelationType:
     NONE = 14
 
     @classmethod
+    def get_string_by_id(cls, ID):
+        if ID == cls.BEFORE:
+            return "BEFORE"
+        elif ID == cls.IS_INCLUDED:
+            return "IS_INCLUDED"
+        elif ID == cls.AFTER:
+            return "AFTER"
+        elif ID == cls.INCLUDES:
+            return "INCLUDES"
+        elif ID == cls.ENDS:
+            return "ENDS"
+        elif ID == cls.ENDED_BY:
+            return "ENDED_BY"
+        elif ID == cls.IBEFORE:
+            return "IBEFORE"
+        elif ID == cls.IAFTER:
+            return "IAFTER"
+        elif ID == cls.SIMULTANEOUS:
+            return "SIMULTANEOUS"
+        elif ID == cls.BEGINS:
+            return "BEGINS"
+        elif ID == cls.BEGUN_BY:
+            return "BEGUN_BY"
+        elif ID == cls.DURING:
+            return "DURING"
+        elif ID == cls.DURING_INV:
+            return "DURING_INV"
+        elif ID == cls.UNKNOWN:
+            return "UNKNOWN"
+        elif ID == cls.NONE:
+            return "NONE"
+
+
+
+    @classmethod
     def get_id(cls, text):
         if text == "BEFORE":
             return cls.BEFORE
