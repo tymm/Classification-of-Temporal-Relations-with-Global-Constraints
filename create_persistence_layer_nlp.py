cd20e49e61f9ee0ba1351a56a6cf4340a6d1cca6
Timm <tymmm1@gmail.com>
2014-08-22 02:22:52 +0200

    Activated logging here.

--- create_persistence_layer_nlp.py
+++ create_persistence_layer_nlp.py
@@ -1,8 +1,12 @@
 from helper.nlp_persistence import Nlp_persistence
 from TrainingSet import TrainingSet
+import logging
+
+# Setting up logging
+logging.basicConfig(filename='creating_nlp_file.log',level=logging.DEBUG)
 
 # Preprocessing nlp information for all relations
-training = TrainingSet(False, "data/training/TE3-Silver-data/", "data/training/TBAQ-cleaned/AQUAINT/", "data/training/TBAQ-cleaned/TimeBank/", "data/test/te3-platinum/")
+training = TrainingSet(False, "data/training/TE3-Silver-data/AFP_ENG_19970401.0006.tml")
 
 persistence = Nlp_persistence()
 persistence.create_persistence(training.relations)
