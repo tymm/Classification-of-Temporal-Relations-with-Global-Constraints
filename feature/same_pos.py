5e413158f58c163b73a4af979ab87b59af0c2080
Timm <tymmm1@gmail.com>
2014-09-29 16:42:34 +0200

    Using pos tags from nlp cache.

--- feature/same_pos.py
+++ feature/same_pos.py
@@ -1,9 +1,17 @@
 class Same_pos:
-    def __init__(self, relation):
+    def __init__(self, relation, nlp_persistence_obj):
         self.relation = relation
+        self.nlp_persistence_obj = nlp_persistence_obj
 
     def is_same(self):
-        if self.relation.source.pos == self.relation.target.pos:
+        if self._get_pos_tag(self.relation.source) == self._get_pos_tag(self.relation.target):
             return True
         else:
             return False
+
+    def _get_pos_tag(self, entity):
+        sentence = entity.sentence
+        word = entity.text
+        pos_tag = self.nlp_persistence_obj.get_pos_tag_for_word(sentence, word)
+
+        return pos_tag
