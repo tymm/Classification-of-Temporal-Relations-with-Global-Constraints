2924d8abbfd31341f4c16bb9fc093ef57e4ae1e4
Timm <tymmm1@gmail.com>
2014-10-01 18:50:54 +0200

    Forgot to add methods which are needed to run stuff on all cores.

---
+++ helper/pickle_methods.py
@@ -0,0 +1,36 @@
+import copy_reg
+import types
+import pickle
+
+def _pickle_method(method):
+    # Author: Steven Bethard
+    # http://bytes.com/topic/python/answers/552476-why-cant-you-pickle-instancemethods
+    func_name = method.im_func.__name__
+    obj = method.im_self
+    cls = method.im_class
+    cls_name = ''
+    if func_name.startswith('__') and not func_name.endswith('__'):
+        cls_name = cls.__name__.lstrip('_')
+    if cls_name:
+        func_name = '_' + cls_name + func_name
+    return _unpickle_method, (func_name, obj, cls)
+
+def _unpickle_method(func_name, obj, cls):
+    # Author: Steven Bethard
+    # http://bytes.com/topic/python/answers/552476-why-cant-you-pickle-instancemethods
+    for cls in cls.mro():
+        try:
+            func = cls.__dict__[func_name]
+        except KeyError:
+            pass
+        else:
+            break
+    return func.__get__(obj, cls)
+
+# This call to copy_reg.pickle allows you to pass methods as the first arg to
+# mp.Pool methods. If you comment out this line, `pool.map(self.foo, ...)` results in
+# PicklingError: Can't pickle <type 'instancemethod'>: attribute lookup
+# __builtin__.instancemethod failed
+
+def activate():
+    copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
