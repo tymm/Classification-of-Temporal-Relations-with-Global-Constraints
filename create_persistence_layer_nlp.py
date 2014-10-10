fc5347abb9c6d259a02489875ede343f4cabea1b
Timm <tymmm1@gmail.com>
2014-10-10 18:59:38 +0200

    Updated arguments.

--- create_persistence_layer_nlp.py
+++ create_persistence_layer_nlp.py
@@ -8,5 +8,5 @@ logging.basicConfig(filename='creating_nlp_file.log',level=logging.DEBUG)
 # Preprocessing nlp information for all relations
 training = TrainingSet(False, "data/training/TE3-Silver-data/", "data/training/TBAQ-cleaned/AQUAINT/", "data/training/TBAQ-cleaned/TimeBank/", "data/test/te3-platinum/")
 
-persistence = Nlp_persistence()
+persistence = Nlp_persistence(fallback=True)
 persistence.create_persistence(training.relations)
