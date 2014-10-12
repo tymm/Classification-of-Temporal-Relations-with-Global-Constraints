4c080d137464643da952e1d7d9c2fd2e015aba4d
Timm <tymmm1@gmail.com>
2014-10-12 20:21:03 +0200

    Forgot to add strings.py...

---
+++ feature/strings.py
@@ -0,0 +1,20 @@
+class Strings:
+    def __init__(self, relation, strings_cache):
+        self.relation = relation
+        self.cache = strings_cache
+
+        try:
+            self.source = self.cache.index(self.relation.source.text)
+        except:
+            # String is not known from training set
+            self.source = len(self.cache) + 1
+
+        try:
+            self.target = self.cache.index(self.relation.target.text)
+        except:
+            # String is not known from training set
+            self.target = len(self.cache) + 1
+
+
+    def get_length(self):
+        return len(strings_cache) + 1
