1b718f40b20988cb9a8210ebe255fb19abc0f786
Timm <tymmm1@gmail.com>
2014-10-12 20:37:51 +0200

    Wrong bounderies.

--- feature/strings.py
+++ feature/strings.py
@@ -7,13 +7,13 @@ class Strings:
             self.source = self.cache.index(self.relation.source.text)
         except:
             # String is not known from training set
-            self.source = len(self.cache) + 1
+            self.source = len(self.cache)
 
         try:
             self.target = self.cache.index(self.relation.target.text)
         except:
             # String is not known from training set
-            self.target = len(self.cache) + 1
+            self.target = len(self.cache)
 
     def get_length(self):
         return len(self.cache) + 1
