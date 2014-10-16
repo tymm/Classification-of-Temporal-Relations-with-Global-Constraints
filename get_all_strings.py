df6e73953f89240b9c0951587b3aef0e7ee961e4
Timm <tymmm1@gmail.com>
2014-10-16 14:41:54 +0200

    Save all tokens as lower text.

--- get_all_strings.py
+++ get_all_strings.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     for text_obj in data.training.text_objects:
         for relation in text_obj.relations:
-            strings.add(relation.source.text)
-            strings.add(relation.target.text)
+            strings.add(relation.source.text.lower())
+            strings.add(relation.target.text.lower())
 
     pickle.dump(strings, open("strings.p", "wb"))
