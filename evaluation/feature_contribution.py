559eba7cd24456fa2800118937910a188455ba72
Timm <tymmm1@gmail.com>
2014-11-04 14:52:51 +0100

    Feature contribution evaluation script.

---
+++ evaluation/feature_contribution.py
@@ -0,0 +1,55 @@
+from Data import Data
+from System import System
+import cPickle as pickle
+
+class Contribution:
+    def __init__(self):
+        self.data = Data()
+
+        self.features_event_event = [["lemma", "token"], ["pos", "same_pos"], ["textual_order", "sentence_distance", "entity_distance"], "tense", "aspect", "class", "polarity", "same_tense", "same_aspect", "same_class", "same_polarity", "dependency_type", "dependency_order", "dependency_is_root", "temporal_signal", "temporal_discourse", "duration"]
+        self.features_event_timex = [["lemma", "token"], "pos", ["textual_order", "sentence_distance", "entity_distance"], "tense", "aspect", "class", "polarity", "dct", "type", "value", "dependency_type", "dependency_order", "dependency_is_root", "temporal_signal", "duration"]
+
+        self.features_event_event_len = len(self.features_event_event)
+        self.features_event_timex_len = len(self.features_event_timex)
+        self.max_len = max(self.features_event_event_len, self.features_event_timex_len)
+
+        self.accuracies_event_event = []
+        self.accuracies_event_timex = []
+
+        self._run_systems()
+
+    def save_accuracies(self):
+        # save accuracies to file
+        pickle.dump((self.accuracies_event_event, self.accuracies_event_timex), open("eval_contribution.p", "wb"))
+
+    def _run_systems(self):
+        for k in range(1, self.max_len+1):
+            # features will contain entries twice which is no problem for System()
+            features = self._feature_series(k, self.features_event_event) + self._feature_series(k, self.features_event_timex)
+
+            system = System(self.data, features)
+            system.create_features()
+            system.train()
+            system.eval(quiet=True)
+
+            self.accuracies_event_event.append(system.evaluation_accuracy_event_event)
+            self.accuracies_event_timex.append(system.evaluation_accuracy_event_timex)
+
+    def _feature_series(self, k, features_list):
+        features = []
+
+        for i, entry in enumerate(features_list):
+            if i < k:
+                if type(entry) == str:
+                    features.append(entry)
+                elif type(entry) == list:
+                    features += entry
+            else:
+                break
+
+        return features
+
+
+if __name__ == "__main__":
+    c = Contribution()
+    c.save_accuracies()
