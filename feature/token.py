9f97922f663227641ddad987cfbae62a20026ed7
Timm <tymmm1@gmail.com>
2014-11-09 18:53:31 +0100

    Also lemma without timex.

--- feature/token.py
+++ feature/token.py
@@ -1,19 +1,27 @@
+from parsexml.event import Event
+
 class Token:
     def __init__(self, relation, strings_cache):
         self.relation = relation
         self.cache = strings_cache.tokens
 
-        try:
-            self.source = self.cache.index(self.relation.source.text.lower())
-        except ValueError:
-            # String is not known from training set
-            self.source = len(self.cache)
+        if type(self.relation.source) == Event:
+            try:
+                self.source = self.cache.index(self.relation.source.text.lower())
+            except ValueError:
+                # String is not known from training set
+                self.source = len(self.cache)
+        else:
+            self.source = len(self.cache) + 2
 
-        try:
-            self.target = self.cache.index(self.relation.target.text.lower())
-        except ValueError:
-            # String is not known from training set
-            self.target = len(self.cache)
+        if type(self.relation.target) == Event:
+            try:
+                self.target = self.cache.index(self.relation.target.text.lower())
+            except ValueError:
+                # String is not known from training set
+                self.target = len(self.cache)
+        else:
+            self.target = len(self.cache) + 2
 
     def get_length(self):
-        return len(self.cache) + 1
+        return len(self.cache) + 2
