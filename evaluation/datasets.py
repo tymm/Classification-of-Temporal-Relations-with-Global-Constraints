02d5d02f821c60f2634c6ffe8b8acc86a68abe34
Timm <tymmm1@gmail.com>
2014-12-17 02:48:54 +0100

    Added script for evaluating bootstrapping of data and different datasets.

---
+++ evaluation/datasets.py
@@ -0,0 +1,44 @@
+from Data import Data
+from System import System
+from TrainingSet import TrainingSet
+import pickle
+
+def run(data):
+    system = System(data)
+
+    system.use_all_features()
+    system.use_feature_selection()
+
+    system.create_features()
+    system.train()
+    system.eval(quiet=True)
+
+    return system.evaluation_accuracy_event_event, system.evaluation_accuracy_event_timex
+
+def get_different_training_data():
+    sets = {}
+
+    timebank = Data()
+    timebank.training = TrainingSet(False, False, "data/training/TBAQ-cleaned/TimeBank/")
+    sets["TimeBank"] = timebank
+
+    tbaq = Data(inverse=False, closure=False)
+    sets["TBAQ"] = tbaq
+
+    tbaq-i = Data(inverse=True, closure=False)
+    sets["TBAQ-I"] = tbaq-i
+
+    tbaq-ic = Data(inverse=True, closure=True)
+    sets["TBAQ-IC"] = tbaq-ic
+
+    return sets
+
+if __name__ == "__main__":
+    accs = {}
+    datas = get_different_training_data()
+
+    for data in datas.items():
+        accs[data[0]] = run(data[1])
+
+    pickle.dump(accs, open("datasets_eval.p", "wb"))
+    print accs
