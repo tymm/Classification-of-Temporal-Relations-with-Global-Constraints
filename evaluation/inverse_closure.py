8e6afdddc9d66a7a63b5bc7d082d993bb9d380b6
Timm <tymmm1@gmail.com>
2015-01-01 23:47:11 +0100

    Typo.

--- evaluation/inverse_closure.py
+++ evaluation/inverse_closure.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     test_inverse_closure = TrainingSet(True, True, "data/test/te3-platinum/")
     test_closure = TrainingSet(False, True, "data/test/te3-platinum/")
 
-    print "Number of relations: " + str(len(test_plain))
-    print "Number of relations for closure: " + str(len(test_closure))
-    print "Number of relations for inverse and closure: " + str(len(test_inverse_closure))
+    print "Number of relations: " + str(len(test_plain.relations))
+    print "Number of relations for closure: " + str(len(test_closure.relations))
+    print "Number of relations for inverse and closure: " + str(len(test_inverse_closure.relations))
