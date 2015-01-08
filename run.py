932323e33ab6c1fee233ff57b76d7af5797812ce
Timm <tymmm1@gmail.com>
2015-01-08 20:44:25 +0100

    Return output tmls and added comments.

--- run.py
+++ run.py
@@ -1,53 +1,26 @@
 from Data import Data
 from System import System
 
+# Create data
 data = Data()
 system = System(data)
 
-"""
-system.use_token()
-system.use_lemma()
-system.use_dependency_is_root()
-system.use_dependency_order()
-system.use_aspect()
-system.use_tense()
-system.use_same_tense()
-system.use_same_aspect()
-system.use_dependency_type()
-
-system.use_dct()
-system.use_type()
-system.use_value()
-system.use_same_polarity()
-system.use_polarity()
-system.use_class()
-system.use_entity_distance()
-system.use_textual_order()
-system.use_duration()
-system.use_duration_difference()
-system.use_same_pos()
-system.use_pos()
-system.use_duration()
-system.use_temporal_signal()
-"""
+# Create features and apply feature selection
 system.use_all_features()
 system.use_feature_selection()
-
 system.create_features()
+
+# Create classifiers
 system.train()
+# Save classifiers to pickle file
 system.save_classifiers()
 
+# Print out results
 event_event, event_timex = system.eval()
 print "Event-Event:"
 print event_event
 print "Event-Timex:"
 print event_timex
 
-system.create_confidence_scores()
-system.apply_global_model()
-
-event_event, event_timex = system.eval_global_model()
-print "Event-Event:"
-print event_event
-print "Event-Timex:"
-print event_timex
+# Create output tml files for evaluation script
+data.test.create_evaluation_files()
