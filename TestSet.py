bd52a94c9fef4f96f0cc6236edb55f221e944635
Timm <tymmm1@gmail.com>
2015-01-10 18:34:40 +0100

    Print out how many relations of each type were changed.

--- TestSet.py
+++ TestSet.py
@@ -22,14 +22,21 @@ class TestSet(Set):
 
     def apply_global_model(self):
         changed = 0
+        ee_changed = 0
+        et_changed = 0
         for text_obj in self.text_objects:
             # Create all optimal relations for text object
             ilp = Constraints(text_obj)
             self.relations_optimized += ilp.get_best_set()
 
-            changed += ilp.get_number_of_relations_changed()
+            tmp_changed, tmp_ee_changed, tmp_et_changed = ilp.get_number_of_relations_changed()
+            changed += tmp_changed
+            ee_changed += tmp_ee_changed
+            et_changed += tmp_et_changed
 
         print "Global model changed %s relations" % changed
+        print "%s relations were event-event relations" % ee_changed
+        print "%s relations were event-timex relations" % et_changed
 
     def get_event_event_targets(self):
         targets = []
